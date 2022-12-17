93482f6351303aaa9c7ef002d40db25af3368275
viniciusFreitas00 <vini.freirefreitas00@gmail.com>
2022-12-17 15:29:33 -0300

    cancel notification use case

--- test/repositories/in-memory-notification-repository.ts
+++ test/repositories/in-memory-notification-repository.ts
@@ -4,7 +4,15 @@ import { NotificationRepository } from '@application/repositories/notification-r
 export class InMomeryNotificationRepository implements NotificationRepository {
   public notifications: Notification[] = [];
 
+  async findById(notificationId: string): Promise<Notification | null> {
+    throw new Error('Method not implemented.');
+  }
+
   async create(notification: Notification) {
     this.notifications.push(notification);
+  }
+
+  async save(notification: Notification): Promise<void> {
+    throw new Error('Method not implemented.');
   }
 }
