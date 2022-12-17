1dfb84a382e5bbf7b352946295d32b363251b449
viniciusFreitas00 <vini.freirefreitas00@gmail.com>
2022-12-16 22:10:00 -0300

    create notification repository

--- src/application/use-case/send-notification.spec.ts
+++ src/application/use-case/send-notification.spec.ts
@@ -1,15 +1,26 @@
+import { Notification } from '../entities/notification';
 import { SendNotification } from './send-notification';
+
+const notifications: Notification[] = [];
+
+const notificationRepository = {
+  async create(notification: Notification) {
+    notifications.push(notification);
+  },
+};
 
 describe('Send Notification', () => {
   it('Should be able to send a notification', async () => {
-    const sendNotification = new SendNotification();
+    const sendNotification = new SendNotification(notificationRepository);
 
-    const { notification } = await sendNotification.execute({
+    await sendNotification.execute({
       recipientId: 'example-recipient-id',
       content: 'Você recebeu uma solicitação de amizade',
       category: 'social',
     });
 
-    expect(notification).toBeTruthy();
+    console.log(notifications);
+
+    expect(notifications).toHaveLength(1);
   });
 });
