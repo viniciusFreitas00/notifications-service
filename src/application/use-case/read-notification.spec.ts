fc41e8c808e7a6e330a0709a141a61e767bfca04
viniciusFreitas00 <vini.freirefreitas00@gmail.com>
2022-12-17 20:30:15 -0300

    add read notification test

--- src/application/use-case/read-notification.spec.ts
+++ src/application/use-case/read-notification.spec.ts
@@ -1,30 +1,30 @@
 import { InMomeryNotificationRepository } from '@test/repositories/in-memory-notification-repository';
-import { CancelNotification } from './cancel-notification';
 import { NotificationNotFound } from './errors/notification-not-found';
 import { makeNotification } from '@test/factories/notification-factory';
+import { ReadNotification } from './read-notification';
 
-describe('Cancel Notification', () => {
-  it('Should be able to cancel a notification', async () => {
+describe('Read Notification', () => {
+  it('Should be able to read a notification', async () => {
     const notificationRepository = new InMomeryNotificationRepository();
-    const cancelNotification = new CancelNotification(notificationRepository);
+    const readNotification = new ReadNotification(notificationRepository);
 
     const notification = makeNotification();
 
     await notificationRepository.create(notification);
 
-    await cancelNotification.execute({ notificationId: notification.id });
+    await readNotification.execute({ notificationId: notification.id });
 
-    expect(notificationRepository.notifications[0].cancelAt).toEqual(
+    expect(notificationRepository.notifications[0].readAt).toEqual(
       expect.any(Date),
     );
   });
 
   it('should not be able to cancel a notification when it does not exist', async () => {
     const notificationRepository = new InMomeryNotificationRepository();
-    const cancelNotification = new CancelNotification(notificationRepository);
+    const readNotification = new ReadNotification(notificationRepository);
 
     expect(async () => {
-      return await cancelNotification.execute({
+      return await readNotification.execute({
         notificationId: 'fake-notification-id',
       });
     }).rejects.toThrow(NotificationNotFound);
