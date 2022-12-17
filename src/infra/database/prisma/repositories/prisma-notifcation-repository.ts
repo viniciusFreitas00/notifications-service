e2ab37196d8282c98478cce53f94ab7585dcafe3
viniciusFreitas00 <vini.freirefreitas00@gmail.com>
2022-12-17 15:04:02 -0300

    create notification mapper

--- src/infra/database/prisma/repositories/prisma-notifcation-repository.ts
+++ src/infra/database/prisma/repositories/prisma-notifcation-repository.ts
@@ -2,21 +2,17 @@ import { Injectable } from '@nestjs/common';
 import { Notification } from '@application/entities/notification';
 import { NotificationRepository } from '@application/repositories/notification-repositories';
 import { PrismaService } from '../prisma.service';
+import { PrismaNotificationMapper } from '../mappers/prisma-notification-mapper';
 
 @Injectable()
 export class PrismaNotificationRepository implements NotificationRepository {
   constructor(private prismaService: PrismaService) {}
 
   async create(notification: Notification): Promise<void> {
+    const raw = PrismaNotificationMapper.toPrisma(notification);
+
     await this.prismaService.notification.create({
-      data: {
-        id: notification.id,
-        recipientId: notification.recipientId,
-        content: notification.content.value,
-        category: notification.category,
-        reatAt: notification.readAt,
-        createdAt: notification.createdAt,
-      },
+      data: raw,
     });
   }
 }
