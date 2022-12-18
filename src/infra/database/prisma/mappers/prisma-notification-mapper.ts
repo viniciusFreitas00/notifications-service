36ed00fd2efde407f522a3de0ae5ab4e6e826001
viniciusFreitas00 <vini.freirefreitas00@gmail.com>
2022-12-17 21:02:11 -0300

    config prisma repository

--- src/infra/database/prisma/mappers/prisma-notification-mapper.ts
+++ src/infra/database/prisma/mappers/prisma-notification-mapper.ts
@@ -1,4 +1,6 @@
+import { Notification as RawNotification } from '@prisma/client';
 import { Notification } from '@application/entities/notification';
+import { Content } from '@application/entities/content';
 
 export class PrismaNotificationMapper {
   static toPrisma(notification: Notification) {
@@ -10,5 +12,19 @@ export class PrismaNotificationMapper {
       reatAt: notification.readAt,
       createdAt: notification.createdAt,
     };
+  }
+
+  static toDomain(raw: RawNotification): Notification {
+    return new Notification(
+      {
+        recipientId: raw.recipientId,
+        content: new Content(raw.content),
+        category: raw.category,
+        createdAt: raw.createdAt,
+        readAt: raw.reatAt,
+        cancelAt: raw.cancelAt,
+      },
+      raw.id,
+    );
   }
 }
